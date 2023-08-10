ca524d8ea4a504acea057dbb5e50adbd08ff3465
Cadozvex <tnga6642@gmail.com>
2023-08-10 10:18:22 +0700

    fix

--- bai2.js
+++ bai2.js
@@ -1,18 +1,18 @@
 //Function to capitalize the first letter
 const capitalizeText = (text) => {
-  var arr = [];
-  var newArr = text.split(" ");
+  var arr = text.split(" ");
+  var newArr = [];
   //Function to split a string into an array, capitalize the first letter, and then concatenate with the remaining letters of the word
-  for (var i = 0; i < newArr.length; i++) {
-    if (newArr[i] !== "") {
-      newArr[i] = newArr[i].charAt(0).toUpperCase() + newArr[i].slice(1);
-      arr.push(newArr[i]);
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] !== "") {
+      arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+      newArr.push(arr[i]);
     }
   }
   //Join the array back together and trim excess spaces
-  text = arr.join(" ");
+  text = newArr.join(" ");
   return text;
-}
+};
 
 //Test
 console.log(capitalizeText("   ah ah đầkh asfkha ma à"));
